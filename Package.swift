d0c68f348c43d3c4a774c0ac1a19d0c416fa13d8
Daniele Saitta <d.saitta@reply.it>
2020-04-04 18:52:57 +0200

    code updated to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
 import PackageDescription
 
-let package = Package(
-    name: "ObjectMapper"
-)
+let package = Package(name: "ObjectMapper",
+                      platforms: [.macOS(.v10_12),
+                                  .iOS(.v10),
+                                  .tvOS(.v10),
+                                  .watchOS(.v3)],
+                      products: [.library(name: "ObjectMapper",
+                                          targets: ["ObjectMapper"])],
+                      targets: [.target(name: "ObjectMapper",
+                                        path: "Source"),
+                                .testTarget(name: "ObjectMapperTests",
+                                            dependencies: ["Alamofire"],
+                                            path: "Tests")],
+                      swiftLanguageVersions: [.v5])
